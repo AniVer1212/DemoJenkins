db893c31e6d1fe34d59c1d03ad5a1ed5618f75a2
ANISHU <ANISHU@ANISHU>
2024-10-07 13:56:18 +0530

    pushed for jenkins

---
+++ FrameworkJenkins/src/test/java/organizationTest/ToCreateOrganizationTest.java
@@ -0,0 +1,9 @@
+package organizationTest;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+		System.out.println("Organization created successfully");
+		System.out.println("Execution ended");
+	}
+}
